2e3e15d2afe4cd47f99220bf38b8162146841e76
PrzemyslawSarnacki <przemos100@gmail.com>
2020-06-06 21:49:29 +0200

    Checking board works

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -27,18 +27,39 @@ def show_board(solved_board):
 def row_valid(solved_board, row, num):
     # Check row
     for i in range(9):
-        if solved_board[row][i] == num:
+        if solved_board[row][i] == num and solved_board[row][i] != 0:
             return False
     return True
 
+def row_duplicates(solved_board, row, num):
+    # Check row
+    count_duplicates = 0
+    for i in range(9):
+        if solved_board[row][i] == num and solved_board[row][i] != 0:
+            count_duplicates += 1    
+    if count_duplicates > 1:
+        return True
+    else:
+        return False
+    
 def column_valid(solved_board, column, num):
     # Check row
     for i in range(9):
-        if solved_board[i][column] == num:
+        if solved_board[i][column] == num and solved_board[i][column] != 0:
             return False
     return True
-    
 
+def column_duplicates(solved_board, column, num):
+    # Check row
+    count_duplicates = 0
+    for i in range(9):
+        if solved_board[i][column] == num and solved_board[i][column] != 0:
+            count_duplicates += 1    
+    if count_duplicates > 1:
+        return True
+    else:
+        return False
+    
 def square_valid(solved_board, position, num):
     # Check box
     square_row = position[0]//3
@@ -46,9 +67,25 @@ def square_valid(solved_board, position, num):
 
     for i in range(square_row*3, square_row*3 + 3):
         for j in range(square_column*3, square_column*3 + 3):
-            if solved_board[i][j] == num:
+            if solved_board[i][j] == num and solved_board[i][j] != 0:
                 return False
     return True
+
+def square_duplicates(solved_board, position, num):
+    # Check box
+    count_duplicates = 0    
+    square_row = position[0]//3
+    square_column = position[1]//3
+
+    for i in range(square_row*3, square_row*3 + 3):
+        for j in range(square_column*3, square_column*3 + 3):
+            if solved_board[i][j] == num and solved_board[i][j] != 0:
+                count_duplicates += 1
+    if count_duplicates > 1:
+        return True
+    else:
+        return False
+    
 
 def find_empty(solved_board, l):
     for i, row_element in enumerate(solved_board):
@@ -63,13 +100,17 @@ def valid(solved_board, position, num):
     if row_valid(solved_board, position[0], num) and column_valid(solved_board, position[1], num) and square_valid(solved_board, position, num):
         return True
 
+def duplicates(solved_board, position, num):
+    if row_duplicates(solved_board, position[0], num) or column_duplicates(solved_board, position[1], num) or square_duplicates(solved_board, position, num):
+        return True
+
 def check_board(solved_board):
-    for i, row_element in enumerate(solved_board):
-        for j, column_element in enumerate(row_element):
-            if valid(solved_board, (i,j), solved_board[i][j]):
-                continue
-            else:
+    # Is board correct?
+    for i in range(9):
+        for j in range(9):
+            if duplicates(solved_board, (i,j), solved_board[i][j]):
                 print(i,j)
+                # print("ok")
                 return False
     return True
 
@@ -89,8 +130,10 @@ def solve(solved_board):
             solved_board[row][column] = 0
     return False
 
-if(solve(board)): 
-    print("thats right")
-    # show_board(solved_board) 
-else: 
-    print("No solution exists")
+if(check_board(board)):        
+    if(solve(board)):
+        print("thats right")
+    else: 
+        print("No solution exists")
+else:
+    print("check duplicates")
