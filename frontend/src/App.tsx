fe9a1575422e225f8ef3c814f5d1b7dfb6fe3ed1
PrzemyslawSarnacki <przemos100@gmail.com>
2020-06-07 21:12:00 +0200

    Primitive grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-    <input></input>
+    var board : number[][] = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+            [5, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 8, 7, 0, 0, 0, 0, 3, 1],
+            [0, 0, 3, 0, 1, 0, 0, 8, 0],
+            [9, 0, 0, 8, 6, 3, 0, 0, 5],
+            [0, 5, 0, 0, 9, 0, 6, 0, 0],
+            [1, 3, 0, 0, 0, 0, 2, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 7, 4],
+            [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var newb : number[] = [3,3,3,3,3]
+    const handleChange = (e: any) => {
+        console.log(e.value)
+
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+             {board.map((row, i) => {
+                 return (<div>
+                     {row.map((element, j) => {
+                         return (
+                             <input defaultValue={element} key={j} onChange={handleChange} style={{ width: "15px" }} />
+                             )
+                    })}
+                 </div>)
+            })}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
