0fe4b17054a093c95b918b0ece2e99eceb3e1cb5
PrzemyslawSarnacki <przemos100@gmail.com>
2020-06-09 21:09:52 +0200

    Updates

---
+++ precache-manifest.5f8f5e715d3b8fe992359b274c42a960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ced408697cd3a81969bf5ac40150c8",
+    "url": "/ReactSudokuSolver/index.html"
+  },
+  {
+    "revision": "56e37dbeb1c8e6787b04",
+    "url": "/ReactSudokuSolver/static/css/main.338d5bd9.chunk.css"
+  },
+  {
+    "revision": "417212b3bcffa4974e19",
+    "url": "/ReactSudokuSolver/static/js/2.d1362c8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactSudokuSolver/static/js/2.d1362c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e37dbeb1c8e6787b04",
+    "url": "/ReactSudokuSolver/static/js/main.d6d7bd64.chunk.js"
+  },
+  {
+    "revision": "4fc009f222b771636285",
+    "url": "/ReactSudokuSolver/static/js/runtime-main.8efca481.js"
+  }
+]);
