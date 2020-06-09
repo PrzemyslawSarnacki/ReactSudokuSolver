95234ef64d5c1da92c7afb3d4cabb0844e61c3da
PrzemyslawSarnacki <przemos100@gmail.com>
2020-06-09 18:21:30 +0200

    Checking for duplicates added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,37 +2,44 @@ import React, { useState } from 'react';
 import './App.css';
 import { ok } from 'assert';
 
-var solvedBoard: number[][] = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-[5, 2, 0, 0, 0, 0, 0, 0, 0],
-[0, 8, 7, 0, 0, 0, 0, 3, 1],
-[0, 0, 3, 0, 1, 0, 0, 8, 0],
-[9, 0, 0, 8, 6, 3, 0, 0, 5],
-[0, 5, 0, 0, 9, 0, 6, 0, 0],
-[1, 3, 0, 0, 0, 0, 2, 5, 0],
-[0, 0, 0, 0, 0, 0, 0, 7, 4],
-[0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
 
 const App = () => {
 
     const [show, setShow] = useState(false);
+    const [duplicate, setDuplicate] = useState([-1,-1]);
+    const [solvedBoard, setSolvedBoard] = useState([[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]);
+    
+        const [board, setBoard] = useState([[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]);
 
-    var board: number[][] = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-    [5, 2, 0, 0, 0, 0, 0, 0, 0],
-    [0, 8, 7, 0, 0, 0, 0, 3, 1],
-    [0, 0, 3, 0, 1, 0, 0, 8, 0],
-    [9, 0, 0, 8, 6, 3, 0, 0, 5],
-    [0, 5, 0, 0, 9, 0, 6, 0, 0],
-    [1, 3, 0, 0, 0, 0, 2, 5, 0],
-    [0, 0, 0, 0, 0, 0, 0, 7, 4],
-    [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
     const handleChange = (i: number, j: number, e: any) => {
         if (e.target.value) {
             board[i][j] = parseInt(e.target.value)
-            solvedBoard = board
-            console.log(solvedBoard)
-            setShow(true);
-            solve();
+            console.log(board)
+            setSolvedBoard(board);
+            console.log(checkBoard());
+            console.log(duplicate)
+        }
+        else if (e.target.value < 0 || e.target.value > 9){
+            board[i][j] = 0
+            setSolvedBoard(board);
         }
         else {
             console.log("wrong")
@@ -43,6 +50,10 @@ const App = () => {
         setShow(true);
         solve();
     }
+    
+    const handleClear = () => {
+        setShow(false);
+    }
 
     const columnValid = (column: number, num: number) => {
 
@@ -52,6 +63,67 @@ const App = () => {
             }
         }
         return true;
+    }
+
+    const columnDuplicates = (column: number, num: number) => {
+        let duplicateCounter: number = 0;
+        let duplicatePosition: number[] = [-1, -1];
+        for (let i = 0; i < 9; i++) {
+            if (solvedBoard[i][column] === num && solvedBoard[i][column] !== 0) {
+                duplicateCounter ++;
+                duplicatePosition = [i, column]
+            }
+        }
+        if (duplicateCounter > 1) {
+            setDuplicate(duplicatePosition);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    const rowDuplicates = (row: number, num: number) => {
+        let duplicateCounter: number = 0;
+        let duplicatePosition: number[] = [-1, -1];
+        for (let i = 0; i < 9; i++) {
+            if (solvedBoard[row][i] === num && solvedBoard[row][i] !== 0) {
+                duplicateCounter ++;
+                duplicatePosition = [row, i]
+            }
+        }
+        if (duplicateCounter > 1) {
+            console.log(duplicatePosition)
+            setDuplicate(duplicatePosition);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    const squareDuplicates = (position: number[], num: number) => {
+        let duplicateCounter: number = 0;
+        let duplicatePosition: number[] = [-1, -1];
+        
+        let squareRow: number = Math.floor(position[0] / 3);
+        let squareColumn: number = Math.floor(position[1] / 3);
+        
+        for (let i = squareRow * 3; i < squareRow * 3 + 3; i++) {
+            for (let j = squareColumn * 3; j < squareColumn * 3 + 3; j++) {
+                if (solvedBoard[i][j] === num && solvedBoard[i][j] !== 0) {
+                    duplicateCounter++;
+                    duplicatePosition = [i, j];
+                }
+            }
+        }
+        if (duplicateCounter > 1) {
+            setDuplicate(duplicatePosition);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     const rowValid = (row: number, num: number) => {
@@ -76,6 +148,7 @@ const App = () => {
         return false;
     }
 
+
     const squareValid = (position: number[], num: number) => {
         let squareRow: number = Math.floor(position[0] / 3);
         let squareColumn: number = Math.floor(position[1] / 3);
@@ -92,6 +165,23 @@ const App = () => {
 
     const valid = (position: number[], num: number) => {
         if (rowValid(position[0], num) && columnValid(position[1], num) && squareValid(position, num)) {
+            return true;
+        }
+    }
+    
+    const checkBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (duplicates([i, j], solvedBoard[i][j])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    const duplicates = (position: number[], num: number) => {
+        if (rowDuplicates(position[0], num) || columnDuplicates(position[1], num) || squareDuplicates(position, num)) {
             return true;
         }
     }
@@ -121,6 +211,7 @@ const App = () => {
         <div className="App">
             <header className="App-header">
                 <div>
+                    {duplicate}
                     {board.map((row, i: number) => {
                         return (<div key={i}>
                             {row.map((element, j: number) => {
@@ -130,7 +221,8 @@ const App = () => {
                             })}
                         </div>)
                     })}
-                    <button onClick={handleClick}>Look</button>
+                    <button onClick={handleClick}>Solve!</button>
+                    <button onClick={handleClear}>Clear</button>
                     <div>
                         {show ? (
                             <div>
@@ -138,7 +230,7 @@ const App = () => {
                                     return (<div key={i}>
                                         {row.map((element, j: number) => {
                                             return (
-                                                <input defaultValue={element} key={j} onChange={(e: any) => handleChange(i, j, e)} style={{ width: "15px" }} />
+                                                <b key={j} style={{ width: "15px" }}>{element}</b>
                                             )
                                         })}
                                     </div>)
